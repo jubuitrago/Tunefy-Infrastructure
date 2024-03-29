e201342117b2d0b8b7ce2c115257fd953094123a
jubuitrago <juan-pablo.buitrago@endava.com>
2024-03-29 09:45:56 -0500

    deleted scripts from repository

--- recipes/k8s_master_start.rb
+++ recipes/k8s_master_start.rb
@@ -2,10 +2,10 @@ bash 'start_kubernetes_cluster' do
     code <<-EOH
         sudo kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 
-        sudo wget https://raw.githubusercontent.com/jubuitrago/Tunefy/main/kubernetes_scripts/app_configs.yaml
-        sudo wget https://raw.githubusercontent.com/jubuitrago/Tunefy/main/kubernetes_scripts/app_secrets.yaml
-        sudo wget https://raw.githubusercontent.com/jubuitrago/Tunefy/main/kubernetes_scripts/backend.yaml
-        sudo wget https://raw.githubusercontent.com/jubuitrago/Tunefy/main/kubernetes_scripts/frontend.yaml
+        sudo wget https://raw.githubusercontent.com/jubuitrago/Tunefy-infrastructure/main/kubernetes_scripts/app_configs.yaml
+        sudo wget https://raw.githubusercontent.com/jubuitrago/Tunefy-infrastructure/main/kubernetes_scripts/app_secrets.yaml
+        sudo wget https://raw.githubusercontent.com/jubuitrago/Tunefy-infrastructure/main/kubernetes_scripts/backend.yaml
+        sudo wget https://raw.githubusercontent.com/jubuitrago/Tunefy-infrastructure/main/kubernetes_scripts/frontend.yaml
         sudo sed -i 's/PRIMARY_DATABASE_IP/$PRIMARY_DATABASE_IP/g' app_configs.yaml
         sudo sed -i 's/PUBLIC_LB_URL/$PUBLIC_LB_URL/g' app_configs.yaml
 
