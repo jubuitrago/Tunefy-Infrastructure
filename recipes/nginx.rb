a1dd6c96329ab32654b4c6e52b48fcde5380c6df
jubuitrago <juan-pablo.buitrago@endava.com>
2024-03-29 13:51:44 -0500

    prueba

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -4,15 +4,10 @@ bash 'install_and_configure_nginx' do
     sudo apt install nginx -y
 
     sudo cat <<EOF > /etc/nginx/sites-available/default
-    http {
-      split_clients "${remote_addr}AAA" $frontend {
-        50%     10.0.0.56:30000;
-        *       10.0.0.69:30000;
-      }
 
       server {
         location / {
-          proxy_pass http://$frontend;
+          proxy_pass http://10.0.0.56;
           proxy_set_header Host $host;
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
