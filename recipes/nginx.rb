bdf5e8aec645ef1f89ba83b11b187a62eda62eba
jubuitrago <juan-pablo.buitrago@endava.com>
2024-03-29 13:39:44 -0500

    edited on nginx

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -3,17 +3,26 @@ bash 'install_and_configure_nginx' do
     sudo apt update
     sudo apt install nginx
     echo '
-    server {
-    
 
-    location / {
-        proxy_pass http://10.0.0.1;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-    }
+http {
+  split_clients "${remote_addr}AAA" $frontend {
+    50%     10.0.0.56:30000;
+    *       10.0.0.69:30000;
+  }
 
-    }' | sudo tee /etc/nginx/sites-available/default >/dev/null
+server {
+  location / {
+    proxy_pass http://$frontend;
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+  }
+}
+
+    ' | sudo tee /etc/nginx/sites-available/default
+
+    sudo systemctl restart nginx
+
   EOH
 end
