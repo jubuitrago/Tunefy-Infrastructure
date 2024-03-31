447b092e56de753d229bfd41bd9b6ef3dd8fef24
jubuitrago <juan-pablo.buitrago@endava.com>
2024-03-31 16:30:44 -0500

    changes on primary database

--- recipes/primary_database.rb
+++ recipes/primary_database.rb
@@ -9,12 +9,12 @@ bash 'install_and_configure_primary_database' do
         sudo sed -i "s/#wal_level = replica/wal_level = replica/" /etc/postgresql/16/main/postgresql.conf
         echo "host all all 0.0.0.0/0 md5" | sudo tee -a /etc/postgresql/16/main/pg_hba.conf > /dev/null
         echo "host replication replicator REPLICA_DATABASE_IPX/32 trust" | sudo tee -a /etc/postgresql/16/main/pg_hba.conf > /dev/null
-        sudo systemctl restart postgresql
         sudo -u postgres psql -c "CREATE DATABASE tunefy_database;"
         sudo -u postgres createuser --replication -P '1231' -e replicator
 
         wget https://raw.githubusercontent.com/jubuitrago/Tunefy/main/database/scripts/init.sql -P /tmp/
         sudo -u postgres psql -d tunefy_database -f /tmp/init.sql
+        sudo systemctl restart postgresql
         #sudo -u postgres psql -d tunefy_database
     EOH
 end
