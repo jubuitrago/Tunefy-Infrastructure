0d2572c05f65ed0b00bb28258e45a12405afcf3a
jubuitrago <juan-pablo.buitrago@endava.com>
2024-04-01 09:48:07 -0500

    Update template

--- modules/instances/scripts/templates/bastion_provision_script_template.sh
+++ modules/instances/scripts/templates/bastion_provision_script_template.sh
@@ -38,6 +38,16 @@ sudo systemctl daemon-reload
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
 
+#Installation of graphana
+sudo apt-get install -y apt-transport-https software-properties-common
+sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get install grafana
+sudo systemctl daemon-reload
+sudo systemctl enable grafana-server.service
+sudo systemctl start grafana-server
+
 #Installation of chef-server and chef-workstation
 sudo hostnamectl set-hostname chef-server
 echo "127.0.0.1 chef-server" | sudo tee -a /etc/hosts
