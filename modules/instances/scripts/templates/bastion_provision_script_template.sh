124286494efb76d75a78929de37cd9c1d7de0c67
jubuitrago <juan-pablo.buitrago@endava.com>
2024-04-01 11:00:37 -0500

    changes on template

--- modules/instances/scripts/templates/bastion_provision_script_template.sh
+++ modules/instances/scripts/templates/bastion_provision_script_template.sh
@@ -43,7 +43,7 @@ sudo apt-get install -y apt-transport-https software-properties-common
 sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
 echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt-get update
-sudo apt-get install grafana
+sudo apt-get install -y grafana
 sudo systemctl daemon-reload
 sudo systemctl enable grafana-server.service
 sudo systemctl start grafana-server
