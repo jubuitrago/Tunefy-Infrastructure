89dcebe49788f500f7efdd899425580122c3a787
jubuitrago <juan-pablo.buitrago@endava.com>
2024-04-02 12:05:07 -0500

    edited recipe

--- recipes/github_runner.rb
+++ recipes/github_runner.rb
@@ -1,7 +1,7 @@
 bash 'install_and_configure_github_runner' do
     code <<-EOH
     sudo apt-get update
-    sudo apt-get install ca-certificates curl
+    sudo apt-get install -y ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -11,7 +11,7 @@ bash 'install_and_configure_github_runner' do
       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     sudo apt-get install -y jq
 
     registration_url="https://api.github.com/repos/jubuitrago/Tunefy/actions/runners/registration-token"
