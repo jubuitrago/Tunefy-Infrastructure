498b08f329fdce7dc76079a0a578b276b407a380
jubuitrago <juan-pablo.buitrago@endava.com>
2024-04-02 13:15:38 -0500

    edited recipe

--- recipes/github_runner.rb
+++ recipes/github_runner.rb
@@ -14,19 +14,21 @@ bash 'install_and_configure_github_runner' do
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     sudo apt-get install -y jq
 
+    useradd -m github
+    usermod -aG sudo github
+    echo "%sudo ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+    sudo su github
+    mkdir actions-runner && cd actions-runner
+    curl -o actions-runner-linux-x64-2.314.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.314.1/actions-runner-linux-x64-2.314.1.tar.gz
+    tar xzf ./actions-runner-linux-x64-2.314.1.tar.gz
+    pwd
+    ls
+
     registration_url="https://api.github.com/repos/jubuitrago/Tunefy/actions/runners/registration-token"
     echo "Requesting registration URL at '${registration_url}'"
     payload=$(curl -sX POST -H "Authorization: token GITHUB_PERSONAL_TOKEN" ${registration_url})
     export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
-
-    cd $HOME
-    mkdir actions-runner
-    cd $HOME/actions-runner
-    curl -o actions-runner-linux-x64-2.314.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.314.1/actions-runner-linux-x64-2.314.1.tar.gz
-    tar xzf ./actions-runner-linux-x64-2.314.1.tar.gz
-
-    pwd
-
+    
     ./config.sh --name Runner --token ${RUNNER_TOKEN} -- labels my-runner --url https://github.com/jubuitrago/Tunefy --work "/work" --unattended
 
     sudo ./svc.sh install
